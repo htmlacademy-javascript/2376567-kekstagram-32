09f583baa1a72f2819f4017959b9539bfcdcce90
Sergey <serezham13@gmail.com>
2024-07-11 21:08:52 +0700

    выполнение задания Правда или действие

---
+++ js/form.js
@@ -0,0 +1,81 @@
+import {isEscKey} from './util.js';
+
+const body = document.querySelector('body');
+
+const imgUploadForm = document.querySelector('.img-upload__form');
+const imgUploadOverlay = document.querySelector('.img-upload__overlay');
+const uploadCancelButton = document.querySelector('#upload-cancel');
+const textHashtags = document.querySelector('.text__hashtags');
+const textDescription = document.querySelector('.text__description');
+// const uploadFile = document.querySelector('input#upload-file');
+
+const closeModalForm = () => {
+  const imgUploadFieldWrapper = document.querySelector('.img-upload__field-wrapper');
+  const errorText = document.querySelector('.pristine-error.text-help');
+  imgUploadOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+
+  uploadCancelButton.removeEventListener('click', onLoadButtonClose);
+
+  document.removeEventListener('keydown', onLoadEscClose);
+
+  textHashtags.removeEventListener('focus', onTextHashtagsFocus);
+
+  textDescription.removeEventListener('focus', onTextHashtagsFocus);
+
+  // uploadFile.value = null;
+
+  imgUploadForm.reset();
+
+  // errorText.textContent = null;
+  imgUploadFieldWrapper.classList.remove('img-upload__field-wrapper--error');
+};
+
+const openModalForm = () => {
+  imgUploadOverlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+
+  uploadCancelButton.addEventListener('click', onLoadButtonClose);
+
+  document.addEventListener('keydown', onLoadEscClose);
+
+  textHashtags.addEventListener('focus', onTextHashtagsFocus);
+
+  textDescription.addEventListener('focus', onDescriptionFocus);
+
+};
+
+const addStopPropagation = (evt) => {
+  if (isEscKey(evt)) {
+    evt.stopPropagation();
+  }
+};
+
+const onChangeForm = () => {
+  openModalForm();
+};
+
+const onLoadButtonClose = () => {
+  closeModalForm();
+};
+
+const onLoadEscClose = (evt) => {
+  if (isEscKey(evt)) {
+    closeModalForm();
+  }
+};
+
+const onTextHashtagsFocus = () => {
+  textHashtags.addEventListener("keydown", addStopPropagation);
+}
+
+const onDescriptionFocus = () => {
+  textDescription.addEventListener("keydown", addStopPropagation);
+}
+
+
+const loadForm = () => {
+  imgUploadForm.addEventListener('change',onChangeForm);
+}
+
+export { loadForm }
